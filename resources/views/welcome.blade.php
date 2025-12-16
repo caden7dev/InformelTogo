d77cd5d03008311719b14ec9dae3588a597c21a5
Caden Ahadzi <ahadzicaden7@gmail.com>
2025-12-16 14:08:34 +0000

    modification de Finance

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Togo Financement, Gestion Simplifiée</title>
+    <title>Togo Finance, Gestion Simplifiée</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
